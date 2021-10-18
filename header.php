c31b7cbe962afbb7d3674f86e6f7bc73ced9e35c
ritikjariya07 <90382367+ritikjariya07@users.noreply.github.com>
2021-10-17 22:03:04 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+<title> Fashion Hub</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="style.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+<script>
+              new WOW().init();
+              </script>
+</head>
+<body>
+<div class="container">	
+	<div style="background-color:white">
+		<div class="col-sm-6" style="text-align:left; padding:20px">
+			<div class="glyphicon glyphicon-envelope"><b>fashion@examples.com</b></div>
+			<div class="glyphicon glyphicon-earphone"><b>2541365897</b></div>
+		</div>
+	</div>
+	<div class="col-sm-6"style="text-align:right; padding:20px">
+		
+		
+		<a href="logfront.php"><div class="glyphicon glyphicon-user" id="login"><b>Login</b></div></a>
+		<a href="createacc.php"><div class="glyphicon glyphicon-lock"><b>Create</b></div></a>
+    &nbsp;<a href="panel.php" ><b> Admin Panel</b></a>
+	</div>
+</div>	
+<nav class="navbar navbar-inverse" style="padding:15px; background-color:#EBA326;">
+    <div class="navbar-header">
+    	<i><b><h2>Fashion Hub</h2></b></i>
+    </div>
+    <div style="margin-left: 24%; margin-top: 1%; letter-spacing: 7px;">
+    	<ul class="nav navbar-nav">
+
+      <li ><a href="npro.php" style="font-size:18px;color:black">Home</a></li>
+      <li><a href="#"style="font-size:18px;color:black">Men Fashion</a></li>
+      <li><a href="#"style="font-size:18px;color:black">Women Fashion</a></li>
+      <li><a href="contact.php"style="font-size:18px;color:black">Contact</a></li>
+          </ul>
+  
+      </div>
+
+</nav>
